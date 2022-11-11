fb4bd76940f2d85ccd7513f802a14660048e7021
Sofia Ergenova <113054061+SofiaErgenova@users.noreply.github.com>
2022-11-11 20:34:08 +0300

    Update Program.cs
    редактирование задачи

--- Task/Program.cs
+++ Task/Program.cs
@@ -8,27 +8,7 @@
 
     string [] symbols = new string [count];
 
-    void CreateArray (string[] array)
-        {
-            for(int i = 0; i < count; i++)
-                {
-                    Console.WriteLine($"Введите элемент массива c индексом {i}: ");
-                    array[i] = Convert.ToString(Console.ReadLine());
-                }
-        }
-
-CreateArray(symbols);
-
-void PrintArray(string [] array)
-{
-    Console.Write($"[");
-    for(int i = 0; i < array.Length-1; i++)
-    {
-        Console.Write($"\"{array[i]}\",");
-    }
-    Console.Write($"\"{array[array.Length-1]}\"]");
-}
-
+ CreateArray(symbols);
  PrintArray(symbols);
  
  int length = 0;
@@ -52,3 +32,23 @@ string [] threeSymbols = new string [length];
         
     }
 PrintArray(threeSymbols);
+
+
+ void CreateArray (string[] array)
+        {
+            for(int i = 0; i < count; i++)
+                {
+                    Console.WriteLine($"Введите элемент массива c индексом {i}: ");
+                    array[i] = Convert.ToString(Console.ReadLine());
+                }
+        }
+
+void PrintArray(string [] array)
+{
+    Console.Write($"[");
+    for(int i = 0; i < array.Length-1; i++)
+    {
+        Console.Write($"\"{array[i]}\",");
+    }
+    Console.Write($"\"{array[array.Length-1]}\"]");
+}        
